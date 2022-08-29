8d353d3c55451c59084c389211f3ba0124deb11a
Mester2001 <98164954+Mester2001@users.noreply.github.com>
2022-08-29 19:25:46 +0300

    Admin.order

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -145,7 +145,7 @@ class AdminController extends Controller
 public function order()
 {   $order=order::all();
 
-    return view ('admin.order',compact('order'));
+    return view ('Admin.order',compact('order'));
 }
 
 public function delivered($id)
